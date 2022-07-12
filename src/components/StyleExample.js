be3baa040de35e20af3fe4b9f9a137ea54e1b659
Caroline <ckmcdon@gmail.com>
2022-07-12 16:17:21 -0400

    changed class names in css

--- src/components/StyleExample.js
+++ src/components/StyleExample.js
@@ -4,7 +4,7 @@ export default function StyleExample({ newImage }) {
   return (
     <div>
       <h1>{title}</h1>
-      <img class="roundCorners" src={imageUrl} alt="roses" />
+      <img class="rounded" src={imageUrl} alt="roses" />
       <p>{description}</p>
       <div class="codeContainer">
         <p class="code">{code}</p>
