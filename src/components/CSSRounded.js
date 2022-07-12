1e67af46836897a9f197e3a38edb69fb9131a77c
Caroline <ckmcdon@gmail.com>
2022-07-12 12:12:50 -0400

    Rounded CSS

--- src/components/CSSRounded.js
+++ src/components/CSSRounded.js
@@ -1,9 +1,18 @@
+import styled from "styled-components"
+
 export default function CSSRounded() {
   return (
     <div>
       <h1>Rounded Corners</h1>
-      <img src="../../public/assets/PZAA6984.JPG" alt="roses" />
+      <RoundedCSSImage></RoundedCSSImage>
       <p>Steps on how to do it</p>
     </div>
   );
 }
+
+const RoundedCSSImage = styled.img.attrs(() => ({
+  src: "../../public/assets/PZAA6984.JPG",
+  alt: "roses",
+}))`
+  border-radius: 8px;
+  `
