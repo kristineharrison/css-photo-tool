0846069d0fa96b1df0cedb83404017b69e47455c
Kristine Kochie <kristinekochie@gmail.com>
2022-07-12 11:46:46 -0400

    Update CSSRounded

--- src/components/CSSRounded.js
+++ src/components/CSSRounded.js
@@ -2,7 +2,7 @@ export default function CSSRounded() {
   return (
     <div>
       <h1>Rounded Corners</h1>
-      <img src=".src/components/assets/PZAA6984.JPG" />
+      <img src="../../public/assets/PZAA6984.JPG" alt="roses" />
       <p>Steps on how to do it</p>
     </div>
   );
