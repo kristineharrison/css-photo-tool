903f91646c0ed9c839cb9c9374d2bd546b3f42f6
Kristine Kochie <kristinekochie@gmail.com>
2022-07-13 15:01:15 -0400

    Add form

--- src/components/StyleExample.js
+++ src/components/StyleExample.js
@@ -3,17 +3,30 @@ import { useState } from "react";
 export default function StyleExample({ newStyle }) {
   const { imageUrl, title, description, code, classStyle } = newStyle;
 
-  const [originalImage, setOriginalImage] = useState('false')
+  const [originalImage, setOriginalImage] = useState("false");
 
   return (
     <div className="example">
       <h1>{title}</h1>
-      {originalImage ? 
-        <img onClick={()=>{setOriginalImage(!originalImage)}} className={classStyle} src={imageUrl} alt="Image Example" />
-        :
-        <img onClick={()=>{setOriginalImage(!originalImage)}} src={imageUrl} alt="Image Example" />
-      }
-      <p>Click to view the original image</p>
+      {originalImage ? (
+        <img
+          onClick={() => {
+            setOriginalImage(!originalImage);
+          }}
+          className={classStyle}
+          src={imageUrl}
+          alt="Image Example"
+        />
+      ) : (
+        <img
+          onClick={() => {
+            setOriginalImage(!originalImage);
+          }}
+          src={imageUrl}
+          alt="Image Example"
+        />
+      )}
+      <p>Click to turn the style on and off.</p>
       <p>{description}</p>
       <div className="code-container">
         <p className="code">{code}</p>
