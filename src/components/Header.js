5312717052cc5d7cd614f683b7682a244e8b88ee
Kristine Kochie <kristinekochie@gmail.com>
2022-07-14 08:50:45 -0400

    Add NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,10 @@
+import NavBar from "./NavBar";
+
 export default function Header() {
-  return <h1>CSS Image Styles</h1>;
+  return (
+    <div>
+      <h1>CSS Image Styles</h1>
+      <NavBar />
+    </div>
+  );
 }
