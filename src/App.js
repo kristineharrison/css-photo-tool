a6844e25c69db6f216b9a6236710697732f10533
Kristine Kochie <kristinekochie@gmail.com>
2022-07-14 22:44:11 -0400

    Move gallery state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import Styles from "./components/Styles";
 import Header from "./components/Header";
 import Gallery from "./components/Gallery";
@@ -6,42 +5,15 @@ import Home from "./components/Home";
 import { Route, Switch } from "react-router-dom";
 
 export default function App() {
-  const [imageArray, setImageArray] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/cssgallery")
-      .then((res) => res.json())
-      .then((data) => setImageArray(data));
-  }, []);
-
-  function addNewImage(newImage) {
-    setImageArray((imageArray) => [newImage, ...imageArray]);
-
-    fetch("http://localhost:3000/cssgallery", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newImage),
-    });
-  }
-
-  function onDelete(id) {
-    const updatedArray = imageArray.filter((image) => image.id !== id);
-    setImageArray(updatedArray);
-  }
-
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path="/styles">
-          <Styles imageArray={imageArray} onDelete={onDelete} />
+          <Styles />
         </Route>
         <Route exact path="/gallery">
-          <Gallery
-            imageArray={imageArray}
-            onDelete={onDelete}
-            addNewImage={addNewImage}
-          />
+          <Gallery />
         </Route>
         <Route exact path="/">
           <Home />
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react";
 import Form from "./Form";
 import GalleryGrid from "./GalleryGrid";
+import Random from "./Random";
 
-export default function Gallery({ imageArray, onDelete, addNewImage }) {
+export default function Gallery() {
+  const [imageArray, setImageArray] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/cssgallery")
+      .then((res) => res.json())
+      .then((data) => setImageArray(data));
+  }, []);
+
+  function addNewImage(newImage) {
+    setImageArray((imageArray) => [newImage, ...imageArray]);
+
+    fetch("http://localhost:3000/cssgallery", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newImage),
+    });
+  }
+
+  function onDelete(id) {
+    const updatedArray = imageArray.filter((image) => image.id !== id);
+    setImageArray(updatedArray);
+  }
+
   return (
     <div>
+      <Random />
       <Form addNewImage={addNewImage} />
       <GalleryGrid imageArray={imageArray} onDelete={onDelete} />
     </div>
